f266e12d8b2fe312dc37686b44e61c3c25eabcbb
omglolkillerx37 <Dheerajthind@gmail.com>
2018-10-07 22:49:37 -0400

    finisehd TDA.py

--- TDA.py
+++ TDA.py
@@ -1,7 +1,7 @@
-def main:
-    fSp,fEp,fDistence = input("(Use the 24 house format in decimals 3:45 -> 15:45 -> 15.45 ) \n Enter your starting time: "),input("Enter your end point: "),input("Total distance traveled in km")
+def main():
+    fSp,fEp,fDistence = float(input("(Use the 24 house format in decimals 3:45 -> 15:45 -> 15.45 ) \nEnter your starting time: ")),float(input("Enter your end point: ")),float(input("Total distance traveled in km: "))
     fTimeTraveled = fEp - fSp
-    print("The average speed was {0} km/h ".format(fTimeTraveled/fDistence))
+    print("The average speed was {0} km/h ".format(fDistence/fTimeTraveled))
 
 
 if __name__ == '__main__':
