5b75146286263c0c0a36f7835cbe5d6b5a577a1f
omglolkillerx37 <Dheerajthind@gmail.com>
2018-10-09 23:11:04 -0400

    New files

--- Cash-Register.py
+++ Cash-Register.py
@@ -1,6 +1,7 @@
 def main():
     sItem = {1:"apple",2:"Chips",3:"Milk",4:"Bread"}
     cashout = []
+    Numitems = []
 
     print("Id   Item \n")
 
@@ -10,6 +11,17 @@ def main():
     while True:
         iId,iAmount = int(input("Enter the item id:  ")),int(input("Enter Amount of items: "))
 
+        cashout.append(iId)
+        Numitems.append(iAmount)
+
+        print("would you'ld you like another item \n")
+
+        respons = input().split()
+
+        if "yes" in respons:
+            continue
+        elif "no" in respons:
+            break
 
 if __name__ == '__main__':
     main()
