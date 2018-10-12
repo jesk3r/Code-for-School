65b74e3a858ac52e79090dac8897602b4e8c8651
omglolkillerx37 <Dheerajthind@gmail.com>
2018-10-11 21:26:44 -0400

    Updating code

--- Cash-Register.py
+++ Cash-Register.py
@@ -1,8 +1,10 @@
 def main():
     sItem = {1:"apple",2:"Chips",3:"Milk",4:"Bread"}
-    iItemPrice = {"apple":1.99,"Chips"}
+    iItemPrice = {"apple":1.99,"Chips":2.99,"Milk":3.5,"Bread":4.99}
     cashout = []
     Numitems = []
+    Totalprice = 0
+    Tax = 0
 
     print("Id   Item \n")
 
@@ -15,24 +17,37 @@ def main():
         iId,iAmount = int(input("Enter the item id:  ")),int(input("Enter Amount of items: "))
 
         cashout.append(iId)
-        Numitems.append(iAmount)
+        Numitems.append(float(iAmount))
 
         print("would you'ld you like another item ")
 
         respons = input().split()
 
         if "no" in respons:
+            print("\n")
             break
 
+    for j in range(0,len(Numitems)):
+        tId = cashout[j]
+        tkey = sItem[tId]
 
 
-def receipt(cout,numi):
-    totalprice = 0
-    try:
-        for
+        Totalprice += iItemPrice[tkey] * Numitems[j]
 
 
 
+
+    Tax += Totalprice * 0.13
+
+    print("Items","        ","Amount","\n")
+
+    for i in range(0,len(cashout)) :
+        tId = cashout[i]
+        print(sItem[tId],"        ",int(Numitems[i]))
+
+    print("\nsubtotal: $ {0}".format(Totalprice))
+    print("Total Tax: $ {0}".format(Tax))
+    print("Amount due: $ {0}".format(Totalprice+Tax))
 
 if __name__ == '__main__':
     main()
