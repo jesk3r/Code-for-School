c13678c01cd219b241e8ca2d70bf278343bbb668
omglolkillerx37 <Dheerajthind@gmail.com>
2018-10-28 00:08:43 -0400

    Updating code

---
+++ RepitionionProg#9.py
@@ -0,0 +1,18 @@
+iTerms = 5
+iXvalue = 2
+
+
+
+def sumofNumbers(terms,x):
+    iTotal = 0
+
+    if terms == 1:
+        return x
+    else:
+        iTotal += sumofNumbers(terms-1,x)
+
+        return x**terms
+
+
+print(sumofNumbers(iTerms+1,iXvalue))
+
